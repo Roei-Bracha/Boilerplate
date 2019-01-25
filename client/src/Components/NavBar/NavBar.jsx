c3b4c283e0b67bbf6a31a462d4be3d2c6ba54bba
Roei-Bracha <roei635@gmail.com>
2019-01-25 13:57:56 +0200

    working on the search style

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,16 +1,23 @@
 import React, { Component, PropTypes } from 'react'
 import "./NavBar.scss";
 import Login from './Login/Login';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar  from '@material-ui/core/Toolbar';
+import Search from './Search/Search';
 class NavBar extends Component {
     render () {
         return (
-            <nav className="NavBar">
-                <div className="leftDiv">LOGO HERE</div>
-                <div className="MiddleDiv"> SEARCH HERE</div>
-                <div className="RightDiv">
-                    <Login/>
-                </div>
-            </nav>
+            <div>
+                <AppBar position="static" className="NavBar">
+                    <Toolbar className="NavBar-Toolbar">
+                    <div className="leftDiv">LOGO HERE</div>
+                    <div className="MiddleDiv"> <Search/></div>
+                    <div className="RightDiv">
+                        <Login/>
+                    </div>
+                    </Toolbar>
+                </AppBar>
+            </div>
         )
     }
 }
