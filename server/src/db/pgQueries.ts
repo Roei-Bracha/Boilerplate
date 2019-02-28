008c4e975a973bb71680d9cd958d7a6a1273cdfb
Roei-Bracha <roei635@gmail.com>
2019-02-28 19:06:27 +0200

    fix configuration and finish the userchack

--- server/src/db/pgQueries.ts
+++ server/src/db/pgQueries.ts
@@ -3,7 +3,7 @@ const pool = pg.getInstance()
 
 export function checkAuth(userName:string, hash:string) : Promise<boolean>{
     return new Promise((resolve,reject)=>{
-        pool.query(`select * from users where user_name=${userName} and password_hash=${hash}`).then((data)=>{
+        pool.query(`select * from users where user_name='${userName}' and password_hash='${hash}'`).then((data)=>{
             if(data.rowCount!==0){
                 resolve(true)
             }
