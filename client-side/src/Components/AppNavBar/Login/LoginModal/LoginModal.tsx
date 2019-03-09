ad556b2fe65fe66070e258fa77c6dade50a1050d
Roei-Bracha <roei635@gmail.com>
2019-03-09 15:57:29 +0200

    fix a little import

--- client-side/src/Components/AppNavBar/Login/LoginModal/LoginModal.tsx
+++ client-side/src/Components/AppNavBar/Login/LoginModal/LoginModal.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Paper from '@material-ui/core/Paper';
-import './LoginModal.scss'
 export default function LoginModal() : JSX.Element{
     return(
         <Paper className={"contentPaper"}>
