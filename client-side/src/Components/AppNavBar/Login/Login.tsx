ae11edda54e0982f983c1dc02db6ad88db8d711a
Roei-Bracha <roei635@gmail.com>
2019-03-23 22:10:42 +0200

    working on the login with goole and the modal

--- client-side/src/Components/AppNavBar/Login/Login.tsx
+++ client-side/src/Components/AppNavBar/Login/Login.tsx
@@ -1,16 +1,17 @@
 import React , {useState} from 'react';
 import Button from '@material-ui/core/Button';
-import Modal from '@material-ui/core/Modal';
+import Dialog from '@material-ui/core/Dialog';
 import LoginModal from './LoginModal/LoginModal';
 export default function Login () : JSX.Element{
     const [open , setOpen] = useState(false)
+
     return (
         <div>
             <Button onClick={()=>{setOpen(true)}} color="inherit">Login / Regiter</Button>
             {open &&
-                <Modal open={open} onClose={()=>{setOpen(false)}} className={"Modal"}>
+                <Dialog open={open} onClose={()=>{setOpen(false)}} className={"Modal"}>
                     <LoginModal/>
-                </Modal>
+                </Dialog>
             }
         </div>
     )
